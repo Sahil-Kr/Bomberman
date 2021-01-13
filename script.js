cd1ec970c0ec71d0811d87cd92ec9831f6d986fe
Sahil <41794114+Sahil-Kr@users.noreply.github.com>
2021-01-14 01:09:08 +0530

    basic functionality done!

--- script.js
+++ script.js
@@ -1,9 +1,51 @@
 const grid = document.querySelector(".grid");
+const scoreBoard = document.querySelector(".game-score");
+let bombTilesNo = new Array();
+let score = 0;
+let gameOn = true;
 
-for (let i = 0; i < 81; i++) {
-  const tile = document.createElement("div");
-  tile.classList.add("grid__tile");
-  grid.appendChild(tile);
-}
+const init = () => {
+  for (let i = 0; i < 81; i++) {
+    const tile = document.createElement("div");
+    tile.classList.add("grid__tile");
+    tile.setAttribute("tile-no", i);
+    grid.appendChild(tile);
+  }
 
-const tiles = document.querySelectorAll(".grid__tile");
+  while (bombTilesNo.length < 10) {
+    let randIndex = Math.trunc(Math.random() * 81);
+    if (!bombTilesNo.includes(randIndex)) bombTilesNo.push(randIndex);
+    //   tiles[randIndex].classList.add("bomb");
+  }
+
+  const tiles = document.querySelectorAll(".grid__tile");
+
+  tiles.forEach(function (tile, index) {
+    tile.addEventListener("click", (e) => {
+      if (!gameOn) return;
+      let tileNo = Number(tile.getAttribute("tile-no"));
+      // console.log(tileNo);
+      if (bombTilesNo.includes(tileNo)) {
+        gameOn = false;
+        bombTilesNo.forEach((num) => {
+          tiles[num].classList.add("bomb");
+        });
+      } else {
+        tiles[index].classList.add("safe");
+        tiles[index].textContent = ++score;
+        scoreBoard.children[0].textContent = `Score : ${score}`;
+      }
+    });
+  });
+};
+
+init();
+
+document.querySelector(".new-game").addEventListener("click", function () {
+  gameOn = true;
+  document.querySelectorAll(".grid__tile").forEach((tile) => tile.remove());
+  scoreBoard.children[0].textContent = `Score : 0`;
+  score = 0;
+  bombTilesNo = new Array();
+  init();
+});
